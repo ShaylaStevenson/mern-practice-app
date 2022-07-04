2589884b8548b68af8a28c8eb94e6509aeafdb1a
Shayla Stevenson <74476502+Shazzyshay@users.noreply.github.com>
2022-07-04 14:30:59 -0700

    first commit

---
+++ client/build/precache-manifest.382980bb2cb15a432672bbf2d307857c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0af282bc07f25e5a1d26",
+    "url": "/mern-practice-app/static/css/main.bedb88c7.chunk.css"
+  },
+  {
+    "revision": "0af282bc07f25e5a1d26",
+    "url": "/mern-practice-app/static/js/main.0715b757.chunk.js"
+  },
+  {
+    "revision": "43760cc95d1a8868807e",
+    "url": "/mern-practice-app/static/js/runtime~main.21aebbde.js"
+  },
+  {
+    "revision": "b833d4679e575f81df15",
+    "url": "/mern-practice-app/static/js/2.c1899c9d.chunk.js"
+  },
+  {
+    "revision": "ab2c3997a259adc09a47f03f7f920b16",
+    "url": "/mern-practice-app/static/media/profilePicture.ab2c3997.jpg"
+  },
+  {
+    "revision": "95d31f2e4ba84f365af98d1a3b1a2564",
+    "url": "/mern-practice-app/index.html"
+  }
+];
